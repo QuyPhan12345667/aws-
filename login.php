9b03158eb53ecdc6fd26660053e6de9a0ea74784
QuyPhan12345667 <133759870+QuyPhan12345667@users.noreply.github.com>
2023-05-20 09:52:30 +0700

    login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 
 // Thiết lập thông tin kết nối đến database
-$servername = "db-server-lab7.c1cn5swaiy4j.ap-southeast-1.rds.amazonaws.com";
+$servername = "db-lab7-quy20cm.cocgl5wbv5ga.ap-southeast-1.rds.amazonaws.com";
 $username = "admin";
 $password = "12345678";
 $dbname = "myDB";
